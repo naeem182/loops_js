aacbce09df23028e39ef3a734d81c05c8d65a37c
naeem <naeemislamjr182@gmail.com>
2023-08-01 02:52:55 +0600

    while loop intro

---
+++ while.js
@@ -0,0 +1,16 @@
+// initiial
+// while (condition) {
+
+//  Increment/decrement
+// }
+
+var gf = 1;
+while (gf < 10) {
+
+    console.log("hey!!baby", gf)
+
+    //gf=gf+1; 
+    // gf += 1
+    gf++;
+
+}
