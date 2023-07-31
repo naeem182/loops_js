f04fd5721c44cb601d32aa7dc306a2d57e594303
naeem <naeemislamjr182@gmail.com>
2023-08-01 03:56:33 +0600

    continue in arrayloop

--- continue.js
+++ continue.js
@@ -2,8 +2,8 @@ var num = [10, 20, 23, 4, 46, 34, 35, 3, 5, 44, 55, 666];
 
 for (var i = 0; i <= num.length; i++) {
 
-    if (num[i] == 34) {//untill find 34 if find then break
-        break;
+    if (num[i] == 34) {
+        continue;
     }
     console.log(num[i])
 }
