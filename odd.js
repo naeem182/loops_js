7669e10d4b2cec5281baee4ecea691b52973b498
naeem <naeemislamjr182@gmail.com>
2023-08-01 03:31:19 +0600

    odd num print by for loop ..while loop....and do while loop.

---
+++ odd.js
@@ -0,0 +1,38 @@
+//odd num print by for loop
+// for (var i = 1; i < 10; i = i + 2) {
+//     console.log("odd num", i)
+// }
+
+
+// for (var i = 0; i < 10; i++) {
+
+//     if (i % 2 != 0) {
+//         console.log("odd num", i)
+//     }
+// }
+
+
+// //odd num print by while loop
+// var i = 0;
+// while (i < 10) {
+//     if (i % 2 != 0) {
+//         console.log("odd num", i)
+//     }
+//     i++
+// }
+
+
+
+// //odd num print by do while loop
+
+console.log("odd num")
+var i = 0;
+
+do {
+
+    if (i % 2 != 0) {
+        console.log(i)
+    }
+    i++;
+}
+while (i < 10)
